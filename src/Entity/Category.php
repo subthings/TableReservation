c2d253f244215e50326a43c6555fa020f75a2bfb
subthings <Annzaychikova@gmail.com>
2019-08-29 17:06:08 +0700

    fixed Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -8,18 +8,18 @@ use App\Entity\Traits\IdentityTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @UniqueEntity("name")
  */
 class Category
 {
     use IdentityTrait;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     * @Assert\Unique
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $name;
 
